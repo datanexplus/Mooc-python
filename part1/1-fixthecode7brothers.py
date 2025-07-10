f308f1f0dc6fe3b9a01f337e06c23fd1fc9ccbbe
Franco AKA Datanexplus (notebook) <franco.delossantoscardozo@alumnos.udemm.edu.ar>
2025-07-10 13:42:01 -0300

    modified

--- part1/1-fixthecode7brothers.py
+++ part1/1-fixthecode7brothers.py
@@ -1,4 +1,5 @@
-'''"Seven Brothers" is one of the first novels ever written in Finnish. It is the story of seven Orphaned Brothers learning to make their way in the world ( read more on Wikipedia ).
+"""
+"Seven Brothers" is one of the first novels ever written in Finnish. It is the story of seven Orphaned Brothers learning to make their way in the world (read more on Wikipedia).
 
 This program is supposed to print out the names of the Brothers in alphabetical order, but it's not working quite right yet. Please fix the program so that the names are printed in the correct order.
 
@@ -8,4 +9,14 @@ print("Eero")
 print("Lauri")
 print("Aapo")
 print("Tuomas")
-print("Timo")'''
+print("Timo")
+"""
+
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
+
